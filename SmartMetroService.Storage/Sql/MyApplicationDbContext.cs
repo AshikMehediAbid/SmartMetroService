f7bdec93abb384ec4a52b2946cacf7a2b49a665e
AshikMehediAbid <ashikmehediabid@gmail.com>
2026-08-12 13:51:54 +0600

    Rename DbSet properties for clarity in DbContext
    Renamed UserTokens to UserOtps and Tokens to RefreshTokens in MyApplicationDbContext to better reflect their purposes and improve code readability.

--- SmartMetroService.Storage/Sql/MyApplicationDbContext.cs
+++ SmartMetroService.Storage/Sql/MyApplicationDbContext.cs
@@ -8,6 +8,6 @@ public class MyApplicationDbContext : DbContext
     public MyApplicationDbContext(DbContextOptions<MyApplicationDbContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
-    public DbSet<UserOTP> UserTokens { get; set; }
-    public DbSet<Token> Tokens { get; set; }
+    public DbSet<UserOTP> UserOtps { get; set; }
+    public DbSet<Token> RefreshTokens { get; set; }
 }
